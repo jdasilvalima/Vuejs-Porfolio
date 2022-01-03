2f5d3cdb0239f8c2a8bf269c744d994f14439589
jdasilvalima <67206954+jdasilvalima@users.noreply.github.com>
2022-01-02 21:52:07 -0500

    added tailwind

--- vue-ts/src/main.js
+++ vue-ts/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from './i18n'
 import router from './router'
+import './assets/tailwind.css'
 
 createApp(App).use(router).use(i18n).mount('#app')
