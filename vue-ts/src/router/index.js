88f1ef2a73dba64707e9430a8a882d70a635f77f
jdasilvalima <67206954+jdasilvalima@users.noreply.github.com>
2022-01-05 22:59:55 -0500

    started not found page

--- vue-ts/src/router/index.js
+++ vue-ts/src/router/index.js
@@ -26,7 +26,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/ProjectsView.vue')
   },
-  //{ path: '*', redirect: '/goal' },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/NotFoundView.vue')
+  },
 ]
 
 const router = createRouter({
